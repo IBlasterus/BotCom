b83be8f21ba3469d23ac836eb13e3d7fbf6a641d
Oleksandr Chornyi <iblasterus@gmail.com>
2018-04-26 00:00:02 +0300

    Start configure Spring

---
+++ src/main/java/ua/iblasterus/botcom/BotConfig.java
@@ -0,0 +1,15 @@
+package ua.iblasterus.botcom;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Spring configuration
+ * 
+ * @author Александр
+ */
+@Configuration
+@ComponentScan
+public class BotConfig {
+    
+}
